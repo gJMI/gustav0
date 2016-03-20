294a60ca0e8bdf9680050c3ed70a7985294cc900
Jan Mikes <jmikes014@me.com>
2016-03-20 21:13:10 +0100

    First one half working version

--- app/app.js
+++ app/app.js
@@ -16,3 +16,8 @@ App = Ember.Application.extend({
 loadInitializers(App, config.modulePrefix);
 
 export default App;
+
+Ember.$.ajaxSetup({
+   headers: {"Authorization": "Bearer demo_b8d3fb54a86b63641727eba34fd638ef",
+ "WEB-API-key": "0bca73a4-0ebb-4837-a841-7dcb189e9c02"}
+});
