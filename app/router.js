baf0954eccc0bb785e3dbb832fd65cac442e92d8
Jan Mikes <jmikes014@me.com>
2016-04-07 06:43:27 +0200

    History I. part

--- app/router.js
+++ app/router.js
@@ -10,6 +10,7 @@ Router.map(function() {
   this.route('profile');
   this.route('overview');
   this.route('account',{ path: '/accounts/:id' });
+  this.route('transactions',{ path: '/accounts/:account/transactions' })
 });
 
 export default Router;
