f38c12450c2b3b8a341a72699371b53112f18aa7
Jan Mikes <jmikes014@me.com>
2016-04-28 08:31:09 +0200

    Domestic II

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -5,6 +5,6 @@ export default DS.RESTAdapter.extend({
   host: 'http://localhost:3000',
   headers: {
     "WEB-API-key":"4d612e35-c45f-4129-b59a-5cafea9fcb6a",
-    "Authorization":"bearer 3/inonbMI60CSSkJFbeaD0WNlz8hsVNQHm9Yc1QznXNZgTW73uO0FnoC4xIuOz483F"
+    "Authorization":"bearer 3/NOL35zfVNayDK6yXnuseO236PYXv410iyaySUgIQX78Tevh1HgjGDE24XTGzMW8N"
   }
 });
