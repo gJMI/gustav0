24efcdeb1894b06db6fae28867ed826b132f32d9
Jan Mikes <jmikes014@me.com>
2016-05-08 20:40:20 +0200

    Domestic payment

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -5,6 +5,6 @@ export default DS.RESTAdapter.extend({
   host: 'http://localhost:3000',
   headers: {
     "WEB-API-key":"4d612e35-c45f-4129-b59a-5cafea9fcb6a",
-    "Authorization":"bearer 3/GKXg6S28i7XpIimbQo2h4zrPLryaZxjcHf6D95lcjXiwfhJEDoXBaOoJmP9VCX5n"
+    "Authorization":"bearer 3/Y37AnC10wRswm671EgEpnMdujA5KtpXtnuQBDvA4LwICggflNr9AM66xLlMplLe5"
   }
 });
--- app/helpers/format-product-name.js
+++ app/helpers/format-product-name.js
@@ -3,8 +3,8 @@ import Ember from 'ember';
 export function formatProductName(params/*, hash*/) {
   var old_name=params[0];
   var new_name = old_name;
-  if (old_name.length > 20) {
-    new_name=old_name.slice(0,20)+"…";
+  if (old_name.length > 25) {
+    new_name=old_name.slice(0,24)+"…";
   }
   return(new_name);
 }
