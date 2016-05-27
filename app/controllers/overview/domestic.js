ba3f986e77d44a11629805d615dbc159b53b87f6
Jan Mikes <jmikes014@me.com>
2016-05-27 23:32:33 +0200

    Validation plus refactoring

--- app/controllers/overview/domestic.js
+++ app/controllers/overview/domestic.js
@@ -10,26 +10,32 @@ export default Ember.Controller.extend(EmberValidations, {
         },
         "model.accountSender": {
             presence: true
+        },
+        "model.receiverBankCode": {
+            presence: true,
+            length: 4,
+            inclusion: { in : ["0100", "0300", "0600", "0710", "0800", "2010", "2020", "2030", "a2060", "2070", "2100", "2200", "2210", "2220", "2240", "2250", "2260", "2310", "2600", "2700", "3030", "3050", "3500", "4000", "4300", "5400", "5500", "5800", "6000", "6100", "6200", "6210", "6300", "6700", "6800", "7910", "7940", "7950", "7960", "7970", "7980", "7990", "8030", "8040", "8060", "8090", "8150", "8200", "8220", "8230", "8240"]
+            }
+        },
+        "model.amountValue": {
+            presence: true,
+            numericality: {
+                onlyInteger: true,
+                greaterThan: 0,
+                lessThanOrEqualTo: 1000
+            }
         }
     },
     actions: {
-        selectSenderAccount: function (acc) {
+        selectSenderAccount: function(acc) {
             Ember.Logger.debug('Account selection changed ' + acc);
             var model = this.get('model');
             model.set("accountSender", acc);
             Ember.Logger.debug('Account model selected' + model.get("accountSender"));
         },
-        sendDomestic: function () {
+        sendDomestic: function() {
 
-            this.validate().then(()=>{
-               Ember.Logger.debug("Validations ok");
-               // put save here 
-            }.bind(this)).catch(()=>{
-               Ember.Logger.debug("Validations nok");
-               Ember.Logger.debug(this.get("errors"));
-               this.set('showErrors',true); 
-            }.bind(this));
-            
+
             var payload = {};
             var model = this.get("model");
 
@@ -54,16 +60,26 @@ export default Ember.Controller.extend(EmberValidations, {
             Ember.Logger.debug("Domestic Model payload: " + JSON.stringify(payload));
 
             var domestic = this.store.createRecord('domestic', payload);
-            
+
             Ember.Logger.debug("Validation state: " + model.get('isValid'));
-            
-            domestic.save().then(()=> {
-                Ember.Logger.debug('Transition to overview');
-                Ember.$("#modal01").css("display", "none");
-                this.transitionToRoute('overview');
-                Ember.Logger.debug(this);             
-            }.bind(this)).catch(Ember.Logger.warn("Something wrong happened in domestic"));
+
+            this.validate().then(() => {
+                Ember.Logger.debug("Validations ok");
+                // put save here 
+                domestic.save().then(() => {
+                    Ember.Logger.debug('Transition to overview');
+                    //Ember.$("#modal01").css("display", "none");
+                    this.transitionToRoute('overview');
+                    Ember.Logger.debug(this);
+                }.bind(this)).catch(Ember.Logger.warn("Something wrong happened in domestic"));
+            }.bind(this)).catch(() => {
+                Ember.Logger.debug("Validations nok");
+                Ember.Logger.debug(this.get("errors"));
+                this.set('showErrors', true);
+            }.bind(this));
+
+
+
         }
     }
 });
-
